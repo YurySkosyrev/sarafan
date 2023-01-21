8246fe375ff31621d68e742dfc32940d3d356f57
Aviaurez <skosyrev-87@mail.ru>
2023-01-21 10:26:52 +0300

    Step 4/22 connecting the database and adding jackson

--- src/main/java/com/sarafan/controller/MessageController.java
+++ src/main/java/com/sarafan/controller/MessageController.java
@@ -1,8 +1,14 @@
 package com.sarafan.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.sarafan.domain.Message;
+import com.sarafan.domain.Views;
 import com.sarafan.exception.NoMessageException;
+import com.sarafan.repo.MessageRepo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,52 +18,45 @@ import java.util.Map;
 @RequestMapping("message")
 public class MessageController {
 
-    private int counter = 4;
+    private final MessageRepo messageRepo;
 
-    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>(){{
-        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First message");}});
-        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second message");}});
-        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third message");}});
-    }};
+    public MessageController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
 
     @GetMapping
-    public List<Map<String, String>> list(){
-        return messages;
+    @JsonView(Views.IdName.class)
+    public List<Message> allMessage(){
+        return messageRepo.findAll();
     }
 
+    // По переменной id, которая приходит на url, Spring сам находит сообщение
     @GetMapping("{id}")
-    public Map<String, String> one(@PathVariable String id){
-        return messages.stream()
-                .filter(messages -> messages.get("id").equals(id))
-                .findFirst()
-                .orElseThrow(() -> new NoMessageException());
-    }
-
-    @PostMapping
-    public Map<String, String>addMessage(@RequestBody Map<String, String> message){
-        message.put("id", String.valueOf(counter++));
-
-        messages.add(message);
-
+    @JsonView(Views.FullMessage.class)
+    public Message getMessage(@PathVariable("id") Message message){
         return message;
     }
 
+    @PostMapping
+    public Message addMessage(@RequestBody Message message){
+        message.setCreationDate(LocalDateTime.now());
+        return messageRepo.save(message);
+    }
+
     @PutMapping("{id}")
-    public Map<String, String> updateMessage(@RequestBody Map<String, String> message,
-                                             @PathVariable String id) {
-        Map<String, String> messageFromDb = one(id);
+    public Message updateMessage(
+            @PathVariable("id") Message messageFromDB,
+            @RequestBody Message message) {
 
-        messageFromDb.putAll(message);
-        messageFromDb.put("id", id);
+        //Spring копирует все поля пришедшего JSONа в сообщение, полученное из БД, кроме id
+        BeanUtils.copyProperties(message, messageFromDB, "id");
 
-        return messageFromDb;
+        return messageRepo.save(messageFromDB);
     }
 
     @DeleteMapping("{id}")
-    public void deleteMessage(@PathVariable String id){
-        Map<String, String> messageFromDb = one(id);
-
-        messages.remove(messageFromDb);
+    public void deleteMessage(@PathVariable("id") Message message){
+        messageRepo.delete(message);
     }
 
 
--- src/main/java/com/sarafan/domain/Message.java
+++ src/main/java/com/sarafan/domain/Message.java
@@ -1,9 +1,13 @@
 package com.sarafan.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table
@@ -11,6 +15,14 @@ import javax.persistence.*;
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonView(Views.Id.class)
     private Long id;
-    private String test;
+    @JsonView(Views.IdName.class)
+    private String text;
+
+    @Column(updatable = false)
+    // Сериализация в Spring идёт с помощью Json, поэтому используем его формат
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonView(Views.FullMessage.class)
+    private LocalDateTime creationDate;
 }
